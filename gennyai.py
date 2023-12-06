0d87257bb31eb3dfde28b6d7b3fca48afbefddc3
sagniho <sagniho1@asu.edu>
2023-12-05 20:41:36 -0500

    updates

--- gennyai.py
+++ gennyai.py
@@ -61,76 +61,7 @@ def send_message_get_response(assistant_id, user_message):
 
 
 def main():
-    User
-Uploaded image
-modify this to have "quick ask" question bubbles (see picture) that disappear after the first time. the quick ask should work the same way as workflow below so dont change that please (with the spinner and question popping up..)
-
-
-import streamlit as st
-import openai
-from openai import OpenAI
-import base64
-import re
-
-st.set_page_config(page_title="Genny AI Website Advisor", page_icon="gen.png")
-# Set your OpenAI API key
-# Access API key from environment variable
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-client = OpenAI(api_key=OPENAI_API_KEY)
-
-def remove_source_tag(text):
-    # Use a regular expression to find and remove the source tag pattern
-    pattern = r"&#8203;``【oaicite:2】``&#8203;"
-    cleaned_text = re.sub(pattern, '', text)
-    return cleaned_text
-
-# Use the Assistant ID from the Assistant you created
-ASSISTANT_ID = st.secrets['GENNY_ASSISTANT_ID_v2']
-with st.sidebar:
-    st.image("bench.png", use_column_width="auto")
-    st.subheader("Welcome to the [Benchmark Gensuite®.](https://benchmarkgensuite.com) Unified, organically developed, and integrated digital solutions for EHS, sustainability, quality, operational risk, product stewardship, supply chain, and ESG disclosure reporting.  ")
-
-# Create columns for the logo and the title
-col1, col2 = st.columns([1, 4])
-
-# In the first column, display the logo
-with col1:
-    st.image('genny.png', width=175)  # Adjust the width as needed
-
-# In the second column, display the title and subtitle
-with col2:
-    st.markdown("<h2 style='margin-top: 0;'>Benchmark Gensuite® Product Advisor</h2>", unsafe_allow_html=True)
-    st.markdown("<p style='margin-top: 0; padding-left: 10px;'>Your interactive Genny AI-powered guide to the Benchmark Gensuite solutions platform</p>", unsafe_allow_html=True)
-
-
-def send_message_get_response(assistant_id, user_message):
-    # Create a new thread
-    thread = client.beta.threads.create()
-
-    # Add user message to the thread
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id, role="user", content=user_message
-    )
-
-    # Run the assistant
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant_id
-    )
-
-    # Retrieve the assistant's response
-    while True:
-        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        if run.status == "completed":
-            messages = client.beta.threads.messages.list(thread_id=thread.id)
-            latest_message = messages.data[0]
-            text = latest_message.content[0].text.value
-            return text
-
-
-
-def main(): 
-    # Initialize messages in session state if not present
+        # Initialize messages in session state if not present
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
 
@@ -142,6 +73,7 @@ def main():
         else:
             with st.chat_message("assistant", avatar="genn.png"):
                 st.write(msg["content"])
+
     # Quick ask questions setup
     if 'quick_ask_shown' not in st.session_state:
         st.session_state['quick_ask_shown'] = True  # Indicates if quick ask should be shown
