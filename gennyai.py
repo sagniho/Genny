3c20b75d283fafd24988b1ca60ce38128065ae28
sagniho <sagniho1@asu.edu>
2023-12-05 20:43:10 -0500

    updates

--- gennyai.py
+++ gennyai.py
@@ -61,7 +61,7 @@ def send_message_get_response(assistant_id, user_message):
 
 
 def main():
-        # Initialize messages in session state if not present
+    # Initialize messages in session state if not present
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
 
@@ -92,23 +92,22 @@ def main():
                 break
     else:
         user_input = st.chat_input(placeholder="Please ask me your question…")
+        # Process user input and get the response
+        if user_input:
+            # Append the user message to the session state
+            st.session_state['messages'].append({'role': 'user', 'content': user_input})
+            # Display the user message
+            with st.chat_message("user", avatar="🧑‍💻"):
+                st.write(user_input)
 
-    # Process user input and get the response
-    if user_input:
-        # Append the user message to the session state
-        st.session_state['messages'].append({'role': 'user', 'content': user_input})
-        # Display the user message
-        with st.chat_message("user", avatar="🧑‍💻"):
-            st.write(user_input)
-
-        # Get the response from the assistant
-        with st.spinner('Working on this for you now...'):
-            response = send_message_get_response(ASSISTANT_ID, user_input)
-            # Append the response to the session state
-            st.session_state['messages'].append({'role': 'assistant', 'content': response})
-            # Display the assistant's response
-            with st.chat_message("assistant", avatar="genn.png"):
-                st.write(response)
+            # Get the response from the assistant
+            with st.spinner('Working on this for you now...'):
+                response = send_message_get_response(ASSISTANT_ID, user_input)
+                # Append the response to the session state
+                st.session_state['messages'].append({'role': 'assistant', 'content': response})
+                # Display the assistant's response
+                with st.chat_message("assistant", avatar="genn.png"):
+                    st.write(response)
 
 
 if __name__ == "__main__":
