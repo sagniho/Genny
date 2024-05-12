3cf799f6db777337461a02a61e52a06865fd9309
sagniho <79772266+sagniho@users.noreply.github.com>
2024-05-12 18:05:42 -0400

    Update gennyai.py

--- gennyai.py
+++ gennyai.py
@@ -34,7 +34,7 @@ def send_message_get_response(assistant_id, user_message, thread_id):
     run = client.beta.threads.runs.create(
         thread_id=thread_id, 
         assistant_id=assistant_id,
-        additional_instructions="This user is: Mukund, the CEO of Benchmark Gensuite, giving his opening keynote presentation on Day 2 (May 15). Have an interactive conversation with him, like you are his co-presenter, while remembering that there is an audience in front of you both. Tasteful puns, simple language."
+        additional_instructions="This user is: Mukund, the CEO of Benchmark Gensuite, giving his opening keynote presentation on Day 2 (May 15). Have an interactive conversation with him, sort of like you're both emceeing. Be witty and have a back and forth with Mukund, while remembering that there is an audience in front of you both. Tasteful puns, simple language."
     )
 
     while True:
